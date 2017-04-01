012bc6b57404264df5319be1d08e9b220e9e956c
Christopher EnyTC <chris@enytc.com>
2017-04-01 03:42:42 -0300

    Update database adapter to support connection pooling

--- adapters/database.go
+++ adapters/database.go
@@ -9,42 +9,93 @@ import (
 	"github.com/chrisenytc/ullli/config"
 )
 
-var connection redis.Conn
+var pool *redis.Pool
+
+func newPool() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			conn, err := redis.DialURL(config.Get().RedisUrl)
+
+			if err != nil {
+				log.Errorf("Fatal error on database connection: %s", err)
+				return nil, err
+			}
+
+			log.Info("Database connection established successfully.")
+
+			return conn, nil
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+
+			_, err := c.Do("PING")
+
+			return err
+		},
+	}
+}
 
 func LoadDatabase() {
-	// Define client
-	conn, err := redis.DialURL(config.Get().RedisUrl)
+	// Define connection pool
+	pool = newPool()
 
-	if err != nil {
-		log.Panicf("Fatal error on database connection: %s", err)
-	}
-
-	connection = conn
-
-	log.Info("Database connection established successfully.")
 	log.Info("The application is now using the database connection.")
 }
 
 func GetConnection() redis.Conn {
-	return connection
+	return pool.Get()
 }
 
 func GetUrl(key string) (string, error) {
-	return redis.String(connection.Do("HGET", key, "url"))
+	conn := GetConnection()
+
+	result, err := redis.String(conn.Do("HGET", key, "url"))
+
+	conn.Close()
+
+	return result, err
 }
 
 func GetUrlData(key string) ([]string, error) {
-	return redis.Strings(connection.Do("HGETALL", key))
+	conn := GetConnection()
+
+	result, err := redis.Strings(conn.Do("HGETALL", key))
+
+	conn.Close()
+
+	return result, err
 }
 
 func CheckShortCode(key string) (bool, error) {
-	return redis.Bool(connection.Do("EXISTS", key))
+	conn := GetConnection()
+
+	result, err := redis.Bool(conn.Do("EXISTS", key))
+
+	conn.Close()
+
+	return result, err
 }
 
 func SaveUrl(key string, url string) (string, error) {
-	return redis.String(connection.Do("HMSET", key, "url", url, "clicks", 0, "created_at", time.Now().Format(time.RFC3339)))
+	conn := GetConnection()
+
+	result, err := redis.String(conn.Do("HMSET", key, "url", url, "clicks", 0, "created_at", time.Now().Format(time.RFC3339)))
+
+	conn.Close()
+
+	return result, err
 }
 
 func CountClick(key string) (int, error) {
-	return redis.Int(connection.Do("HINCRBY", key, "clicks", 1))
+	conn := GetConnection()
+
+	result, err := redis.Int(conn.Do("HINCRBY", key, "clicks", 1))
+
+	conn.Close()
+
+	return result, err
 }
